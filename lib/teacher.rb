2434fa8b974791192dde2609ce555f3236fafa85
connor worthen <connorw220@protonmail.com>
2019-11-17 21:15:24 +0000

    Automatically backed up by Learn

--- lib/teacher.rb
+++ lib/teacher.rb
@@ -2,8 +2,9 @@ class Teacher < User
 
   KNOWLEDGE = ["a String is a type of data in Ruby", "programming is hard, but it's worth it", "javascript async web request", "Ruby method call definition", "object oriented dog cat class instance", "class method class variable instance method instance variable", "programming computers hacking learning terminal", "bash Ruby rvm update certs"]
 
-  def teach(array)
-    @array = KNOWLEDGE
+  def teach
+    knowledge.sample do |KNOWLEDGE|
+      return 
   end
 end
-KNOWLEDGE.sample
+
